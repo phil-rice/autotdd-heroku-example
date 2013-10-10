6b5d78a2e8d27076becae2ee60a3bae37f3ce44f
Phil <phil.rice@iee.org>
2013-10-10 13:43:01 +0100

    Adding AutoTdd to build

--- build.sbt
+++ build.sbt
@@ -6,8 +6,12 @@ name := "hello"
 
 version := "1.0"
 
-scalaVersion := "2.9.2"
+scalaVersion := "2.10.1"
 
 resolvers += "twitter-repo" at "http://maven.twttr.com"
 
-libraryDependencies ++= Seq("com.twitter" % "finagle-core" % "1.9.0", "com.twitter" % "finagle-http" % "1.9.0")
+libraryDependencies ++= Seq(
+	"com.twitter" % "finagle-core" % "1.9.0", 
+	"com.twitter" % "finagle-http" % "1.9.0",
+	"org.autotdd" %% "engine" % "1.1.0"
+)
--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -1,6 +1,6 @@
-import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
+import org.jboss.netty.handler.codec.http.{ HttpRequest, HttpResponse }
 import com.twitter.finagle.builder.ServerBuilder
-import com.twitter.finagle.http.{Http, Response}
+import com.twitter.finagle.http.{ Http, Response }
 import com.twitter.finagle.Service
 import com.twitter.util.Future
 import java.net.InetSocketAddress
@@ -9,7 +9,7 @@ import util.Properties
 object Web {
   def main(args: Array[String]) {
     val port = Properties.envOrElse("PORT", "8080").toInt
-    println("Starting on port:"+port)
+    println("Starting on port:" + port)
     ServerBuilder()
       .codec(Http())
       .name("hello-server")
@@ -17,13 +17,16 @@ object Web {
       .build(new Hello)
     println("Started.")
   }
+  val engine = org.autotdd.engine.Engine[Int, String]().withDefaultCode((i: Int) => "Hello World(" + i + ")").build
 }
 
 class Hello extends Service[HttpRequest, HttpResponse] {
+  var i = 0
   def apply(req: HttpRequest): Future[HttpResponse] = {
     val response = Response()
     response.setStatusCode(200)
-    response.setContentString("Hello World")
+    i += 1
+    response.setContentString(Web.engine(i))
     Future(response)
   }
 }
